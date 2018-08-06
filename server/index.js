656aa8d226bde5b5abc1d8044904336fb2e1b8b6
shanellewebster <webstershanelle@gmail.com>
2018-08-05 20:07:12 -0500

    tasks coming in

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-// const tc = require("./controller");
+const tc = require("./controller");
 const express = require("express");
 const bodyParser = require("body-parser");
 const massive = require("massive");
@@ -92,6 +92,9 @@ function authenticated(req, res, next) {
     res.sendStatus(401);
   }
 }
+
+//TASK ENDPOINTS
+app.get("/api/tasks", tc.getAllTasks);
 
 const port = process.env.port || 4000;
 app.listen(port, () => {
