7f2a027c553ec63383343dd24d6d9a6a965294df
Shanelle Webster <shanellewebster@Shanelles-MacBook-Pro.local>
2018-09-10 22:20:50 -0500

    started calendar

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,11 +1,35 @@
 import React, { Component } from "react";
+import dateFns from "date-fns";
 
 export default class Calendar extends Component {
   constructor() {
     super();
+
+    this.state = {
+      currentMonth: new Date(),
+      selectedDate: new Date()
+    };
   }
 
+  renderHeader() {}
+
+  renderDays() {}
+
+  renderCells() {}
+
+  onDateClick = day => {};
+
+  nextMonth = () => {};
+
+  prevMonth = () => {};
+
   render() {
-    return <div>Calendar</div>;
+    return (
+      <div className="calendar">
+        {this.renderHeader()}
+        {this.renderDays()}
+        {this.renderCells()}
+      </div>
+    );
   }
 }
