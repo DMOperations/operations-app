5ed1457f54130b6e99f2a4b1984177dab05a10e3
Matt Hopkins <matthops@gmail.com>
2018-09-20 09:55:42 -0500

    added checkbox functionality

--- src/components/List/Task.js
+++ src/components/List/Task.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 import moment from "moment";
 import Moment from "react-moment";
+import Checkbox from "@material-ui/core/Checkbox";
 import "./Task.css";
 
 class Task extends Component {
@@ -33,6 +34,11 @@ class Task extends Component {
     return (
       <div className="task">
         <div className="task_left">
+          <Checkbox
+            checked={this.state.status}
+            onClick={this.updateStatus}
+            value={this.state.status}
+          />
           <div className="task_cohort">{this.props.cohortId} -</div>
           <div className="task_headline">{this.props.task}</div>
           <div className="task_cohort">
