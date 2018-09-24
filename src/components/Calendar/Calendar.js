fdd7ef5992aeff5fb6631716edf8c1abd81b23c1
Shanelle Webster <webstershanelle@gmail.com>
2018-09-24 14:04:49 -0500

    filter working

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { connect } from "react-redux";
 import FullCalendar from "fullcalendar-reactwrapper";
 import "fullcalendar-reactwrapper/dist/css/fullcalendar.min.css";
 
-export default class Calendar extends Component {
+class Calendar extends Component {
   constructor() {
     super();
 
     this.state = {
-      activies: []
+      activies: [],
+      select: "All"
     };
+    this.handleCalendar = this.handleCalendar.bind(this);
   }
 
   componentDidMount() {
@@ -18,8 +21,21 @@ export default class Calendar extends Component {
       .then(results => this.setState({ activies: results.data }));
   }
 
+  async handleCalendar(event) {
+    await this.setState({ select: event.target.value });
+    if (this.state.select == "All") {
+      axios
+        .get("/api/allTasks")
+        .then(results => this.setState({ activies: results.data }));
+    } else if (this.state.select == "One") {
+      axios
+        .get(`/api/allTasksByUser?user=${this.props.user.position}`)
+        .then(results => this.setState({ activies: results.data }));
+    }
+  }
+
   render() {
-    console.log(this.state);
+    console.log(this.props);
     const activiesList = this.state.activies.map(e => {
       return {
         title: `
@@ -31,31 +47,44 @@ export default class Calendar extends Component {
     });
 
     return (
-      <div className="calendar">
-        <FullCalendar
-          id="your-custom-ID"
-          header={{
-            left: "month,basicWeek,basicDay",
-            center: "title",
-            right: "today myCustomButton prev,next"
-          }}
-          defaultDate={new Date()}
-          displayEventEnd={true}
-          selectable={true}
-          dragScroll={true}
-          editable={true}
-          eventTextColor="#000000"
-          eventBorderColor="#e6e6e6"
-          eventClick={function(calEvent, jsEvent, view) {
-            alert("Event: " + calEvent.title);
-          }}
-          navLinks={true}
-          eventLimit={true}
-          events={activiesList}
-          height={900}
-          // theme="bootstrap3"
-        />
+      <div>
+        <h1>Narrow it down:</h1>
+        <div>
+          <select value={this.state.select} onChange={this.handleCalendar}>
+            <option value="All">All</option>
+            <option value="One">Only Mine</option>
+          </select>
+        </div>
+        <div className="calendar">
+          <FullCalendar
+            id="your-custom-ID"
+            header={{
+              left: "month,basicWeek,basicDay",
+              center: "title",
+              right: "today myCustomButton prev,next"
+            }}
+            defaultDate={new Date()}
+            displayEventEnd={true}
+            selectable={true}
+            dragScroll={true}
+            editable={true}
+            eventTextColor="#000000"
+            eventBorderColor="#e6e6e6"
+            eventClick={function(calEvent, jsEvent, view) {
+              alert("Event: " + calEvent.title);
+            }}
+            navLinks={true}
+            eventLimit={true}
+            events={activiesList}
+            height={900}
+            // theme="bootstrap3"
+          />
+        </div>
       </div>
     );
   }
 }
+
+const mapStateToProps = state => state;
+
+export default connect(mapStateToProps)(Calendar);
