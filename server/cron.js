5acf02884ab71a161481a6fc96c3bd80f9a0bbf2
Matt Hopkins <matthops@gmail.com>
2018-10-03 10:31:39 -0500

    got cron job working, commented on server

--- server/cron.js
+++ server/cron.js
@@ -1,12 +1,18 @@
 //CRON REQUIREMENTS
 const { getWeeklyTasks } = require("./controller");
-const CronJob = require("cron").CronJob;
+const { CronJob } = require("cron");
 
 const job = async (req, res, next) =>
-  new CronJob("* * * * * *", function() {
-    console.log("CXRON");
-    // getWeeklyTasks(req, res, next);
-    // const myData = await req.app.get('db').getTheStuff();
-  });
+  new CronJob(
+    "* * * * * *",
+    function() {
+      console.log("CXRON");
+      // getWeeklyTasks(req, res, next);
+      // const myData = await req.app.get('db').getTheStuff();
+    },
+    null,
+    true,
+    "America/Chicago"
+  );
 
-module.exports = job;
+module.exports = { job };
