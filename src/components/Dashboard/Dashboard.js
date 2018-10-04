37ce5dc9d3e3476ecc26c41affb7d822508ae9b4
Shanelle Webster <webstershanelle@gmail.com>
2018-10-03 22:16:39 -0500

    animate

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import "./Dashboard.css";
 import List from "../List/List";
 import routes from "../../routes.js";
 import { withRouter } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class Dashboard extends Component {
   constructor() {
@@ -12,6 +13,9 @@ class Dashboard extends Component {
   render() {
     return (
       <div className="dash_main">
+        <Link to={`/addnewtask/${this.props.match.params.id}`}>
+          Add New Task
+        </Link>
         <div>
           <List />
         </div>
