2acbffb6190eeaf946b5f017aab379063b201d84
Matt Hopkins <matthops@gmail.com>
2018-10-06 13:08:34 -0500

    cron job fully operational

--- server/cron.js
+++ server/cron.js
@@ -5,10 +5,11 @@ const moment = require("moment");
 
 const job = db => {
   return new CronJob(
-    "* * * * * *",
+    "0 0 1 * * 0",
     async function() {
       try {
         const myData = await db.get_weekly_tasks();
+
         myData.forEach(function(part, index) {
           const dayOfWeek = myData[index].task_date;
           const taskDateCalc = dayOfWeek =>
@@ -16,8 +17,20 @@ const job = db => {
               .startOf("isoweek")
               .add(dayOfWeek, "days")
               .format("YYYY-MM-DD");
-
           myData[index].task_date = taskDateCalc(dayOfWeek);
+        });
+        // console.log(myData);
+
+        myData.forEach(val => {
+          db.add_weekly_activities([
+            val.task_date,
+            val.task_body,
+            val.task_headline,
+            val.position,
+            val.status,
+            val.assigned_to
+          ]);
+          console.log("worked");
         });
       } catch (e) {
         console.log(e);
