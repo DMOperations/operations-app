c3d442afc94530962abb8500eb49039ee7f4238a
Matt Hopkins <matthops@gmail.com>
2018-10-06 13:40:19 -0500

    updated cronjob to add weekly task id

--- server/cron.js
+++ server/cron.js
@@ -5,7 +5,8 @@ const moment = require("moment");
 
 const job = db => {
   return new CronJob(
-    "0 0 1 * * 0",
+    // "0 0 1 * * 0",
+    "1 * * * * *",
     async function() {
       try {
         const myData = await db.get_weekly_tasks();
@@ -28,7 +29,8 @@ const job = db => {
             val.task_headline,
             val.position,
             val.status,
-            val.assigned_to
+            val.assigned_to,
+            val.cohort_id
           ]);
           console.log("worked");
         });
