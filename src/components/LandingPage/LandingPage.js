33434b9449661d22bc75c60a90852a7e29779914
Shanelle Webster <webstershanelle@gmail.com>
2018-10-16 16:21:11 -0500

    to build

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 import WideLogo from "./WideBlueBlack.png";
-import './LandingPage.css';
+import "./LandingPage.css";
 
 export default class LandingPage extends Component {
   render() {
     return (
-      <div className='landing_background'>
+      <div className="landing_background">
         <div className="landing_page">
           <img src={WideLogo} />
           <h1>Operations Checklist</h1>
-          <a href="http://localhost:4000/login">LOGIN</a>
+          <a href={process.env.REACT_APP_LOGIN}>LOGIN</a>
         </div>
       </div>
     );
